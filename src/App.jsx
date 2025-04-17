353a74287be5ffe569d78039fee9d79ae9ed4890
WebsterFever <webstersevenlanguages@gmail.com>
2025-04-17 13:51:40 -0400

    Your commit message

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { EntriesList } from "./components/EntriesList";
+import { Header } from "./components/Header";
+import { RegisterSection } from "./components/RegisterSection";
+import { ResumeSection } from "./components/ResumeSection";
+import "./styles/index.scss";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <RegisterSection />
+      <ResumeSection />
+      <EntriesList />
+    </>
+  );
+}
+
+export default App;
